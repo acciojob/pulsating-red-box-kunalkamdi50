85f93c025183badbd203ca0a9bb4a793c4bb662c
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-07-26 11:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+it("should have the pulse animation applied", () => {
+  cy.get(".box").should(($box) => {
+    const animation = $box.css("animation");
+    expect(animation).to.include("pulse");
+  });
+});
